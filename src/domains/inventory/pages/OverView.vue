2a765390b43b8012668977c2542a6ed3773233ce
janderkkotlarski <j.d.kotlarski@gmx.com>
2026-07-28 15:12:30 +0200

    It helps to install vue-routervia npmcorrectly and not just vue router

---
+++ src/domains/inventory/pages/OverView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Inventory from '../../../components/InventoryTable.vue';
+</script>
+
+<template>
+    Overzicht
+    <br />
+    <br />
+
+    <Inventory />
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import {createApp} from 'vue';
-import './style.css';
 import {router} from './router';
-import App from './App.vue';
+import App from './app.vue';
 
 createApp(App).use(router).mount('#app');
