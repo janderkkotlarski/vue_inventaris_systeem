5ce692e5ca6a62d37d7f28563104b5b92b4cd314
janderkkotlarski <j.d.kotlarski@gmx.com>
2026-09-08 15:02:18 +0200

    no need for computed outside the store in order to filter and map while keeping tabs on the deeply reactive changes

--- src/domains/inventory/components/OrderTable.vue
+++ src/domains/inventory/components/OrderTable.vue
@@ -1,41 +1,9 @@
 <script setup lang="ts">
-import {ref, computed} from 'vue';
-
-import {getAllInventory, getLowInventory} from '../store.js';
-
-import type {Product, Order} from '../../../components/library';
+import type {Order} from '../../../components/library';
 
 const props = defineProps<{
-    orderList: Product[];
+    orderList: Order[];
 }>();
-
-const productList = getAllInventory;
-
-const testList = getLowInventory;
-
-// const productscopy = ref<Product[]>({...props.orderList});
-
-const product2order = (prod: Product): Order => {
-    return {id: prod.id, product: prod, amount: prod.minimumAmount - prod.actualAmount};
-};
-
-// TODO: het lijkt mij gemakkelijker een computed property van orders te maken, waarbij je een array.map functie gebruikt voor de conversie
-// van products naar orders
-// const minimumOrdering = () => {
-//     let orders: Order[] = [];
-
-//     for (const prod of props.orderList) {
-//         // const order: Order = {id: prod.id, product: prod, amount: prod.minimumAmount - prod.actualAmount};
-
-//         orders.push(product2order(prod));
-//     }
-
-//     return orders;
-// };
-
-const orderMin = computed<Order[]>(() => props.orderList.map(product2order));
-
-// const orders = ref<Order[]>(minimumOrdering());
 
 const emit = defineEmits(['submit']);
 
@@ -52,7 +20,7 @@ const submitOrders = () => {
             <th>Minimum</th>
             <th>Bestellen</th>
         </tr>
-        <tr v-for="order in orderMin" :key="order.id">
+        <tr v-for="order in orderList" :key="order.id">
             <td>{{ order.product.name }}</td>
             <td>{{ order.product.actualAmount }}</td>
             <td>{{ order.product.minimumAmount }}</td>
@@ -63,13 +31,4 @@ const submitOrders = () => {
     </table>
 
     <button type="button" @click="submitOrders">Bestel</button>
-    <br />
-    <br />
-
-    <div>{{ productList }}</div>
-
-    <br />
-    <br />
-
-    <div>{{ testList }}</div>
 </template>
--- src/domains/inventory/pages/OrDer.vue
+++ src/domains/inventory/pages/OrDer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {computed} from 'vue';
+import type {Product, Order} from '../../../components/library';
 
 import {getAllInventory} from '../store.js';
 
@@ -7,7 +7,17 @@ import {router} from '../../../router/index.ts';
 
 import OrderTable from '../components/OrderTable.vue';
 
-const products = getAllInventory;
+const product2order = (prod: Product): Order => {
+    return {id: prod.id, product: prod, amount: prod.minimumAmount - prod.actualAmount};
+};
+
+const lowInvProducts = getAllInventory.filter(prod => prod.actualAmount < prod.minimumAmount);
+
+// const orderList = lowInvProducts.map(product2order);
+
+const orderList = getAllInventory.filter(prod => prod.actualAmount < prod.minimumAmount).map(product2order);
+
+// const orderList = getAllInventory.filter(prod => prod.actualAmount < prod.minimumAmount);
 
 const ordering = () => {
     router.push({name: 'inventory.overview'});
